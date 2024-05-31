e2777a160ef7c17ce90de381d160b87b34f809a0
yasu-da-joe <yasu-da-joe>
2024-05-31 17:04:25 +0900

    production.rb更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.require_master_key = ENV["SECRET_KEY_BASE_DUMMY"].nil?
 end
