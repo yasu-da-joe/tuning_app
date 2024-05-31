de01b09268896c9a619230292d58ed37ef11d9a4
yasu-da-joe <yasu-da-joe>
2024-05-31 20:43:33 +0900

    add render.yaml

--- config/initializers/secret_key.rb
+++ config/initializers/secret_key.rb
@@ -1,3 +1,5 @@
 if defined?(Devise)
-  Devise.secret_key = ENV['DEVISE_SECRET_KEY'] || 'e4b7d3c9a0f96a8f'
+  devise_key = ENV['DEVISE_SECRET_KEY'] || 'e4b7d3c9a0f96a8f'
+  raise "DEVISE_SECRET_KEY must be exactly 16 bytes" unless devise_key.bytesize == 16
+  Devise.secret_key = devise_key
 end
