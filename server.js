e243ec712c1fe188c5e94a7a691c4c3b6e1558f2
yasu-da-joe <yasu-da-joe>
2024-06-03 19:28:37 +0900

    Add server.js and start script

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware settings
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Server start
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
