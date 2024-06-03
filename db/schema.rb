2a4d04e0ca3a61e73adbdfacd78d87c239f021cc
yasu-da-joe <yasu-da-joe>
2024-06-03 19:20:37 +0900

    index.html追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_31_093721) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_100224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_31_093721) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
   end
 
 end
